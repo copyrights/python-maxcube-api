88b802c7b977323c4472e6ddf58b721d83f28302
mike-k68 <61347714+mike-k68@users.noreply.github.com>
2021-01-12 08:54:40 +0100

    Update room.py

--- maxcube/room.py
+++ maxcube/room.py
@@ -2,3 +2,18 @@ class MaxRoom(object):
     def __init__(self):
         self.id = None
         self.name = None
+        self.group_rf_address = None
+        self.has_changed = False
+        self.day_comfort = False
+
+    def is_room(self):
+        return True
+
+    def set_changed(self):
+        self.has_changed = True
+
+    def get_changed(self):
+        value = self.has_changed
+        self.has_changed = False
+        return value
+    
