2f4ad81e03556ebe6bff152fbbcd283249405e6f
mike-k68 <61347714+mike-k68@users.noreply.github.com>
2021-01-12 08:56:28 +0100

    Update wallthermostat.py

--- maxcube/wallthermostat.py
+++ maxcube/wallthermostat.py
@@ -8,6 +8,9 @@ class MaxWallThermostat(MaxDevice):
         self.eco_temperature = None
         self.max_temperature = None
         self.min_temperature = None
-        self.actual_temperature = None
+        self.programme = None
         self.target_temperature = None
+        self.actual_temperature = None
+        self.locked = None
         self.mode = None
+        self.vacation_until = None  # if mode is vacation: end date in format YYYY-MM-DD hh:mm
