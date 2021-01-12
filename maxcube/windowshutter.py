384f6ccd11ad80586dbad9f08a720ada12e6f9a0
mike-k68 <61347714+mike-k68@users.noreply.github.com>
2021-01-12 08:57:02 +0100

    Update windowshutter.py

--- maxcube/windowshutter.py
+++ maxcube/windowshutter.py
@@ -4,5 +4,4 @@ from maxcube.device import MaxDevice
 class MaxWindowShutter(MaxDevice):
     def __init__(self):
         super(MaxWindowShutter, self).__init__()
-        self.is_open = False
-        self.initialized = None
+        self.is_open = None
